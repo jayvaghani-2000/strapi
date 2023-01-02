12bbb36c502a6e97733f529636d05f627f39eca9
jayvaghani-2000 <jayvaghani2000@gmail.com>
2023-01-02 23:07:21 +0530

    add postgre

--- paytm-gatway/config/database.js
+++ paytm-gatway/config/database.js
@@ -1,11 +1,15 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
+  defaultConnection: "default",
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "Payment"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "Geet@2000"),
+      schema: env("DATABASE_SCHEMA", "public"),
+      ssl: false,
     },
-    useNullAsDefault: true,
   },
 });
