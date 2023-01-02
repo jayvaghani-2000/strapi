55ff08213d0d668bca4483c86de00cccbf0bd2f8
jayvaghani-2000 <jayvaghani2000@gmail.com>
2023-01-03 00:28:50 +0530

    add schema

--- paytm-gatway/src/index.js
+++ paytm-gatway/src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   /**
@@ -18,3 +18,5 @@ module.exports = {
    */
   bootstrap(/*{ strapi }*/) {},
 };
+
+// ReadOnly e999f9dc929b985eb38c2ccb2dd2b9e46bd3e81d4ece8bd430cf0c729e939522eb5ce3798e5029d1c158b5a8067c58f8b5eed15bb9ef42732ac992b1d1ec1eeff404b302e41ccdd2d59b6a00193e0c9c08e764bae8f918a2c4dc264513192d73789adf2b3a5d6b8b2e1f9045936206b90293014d51efa0272d5723255ff78652
