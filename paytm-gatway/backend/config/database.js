562d339d58218731cd1b5533e742b19ce673431e
jay vaghani <jayvaghani2000@gmail.com>
2023-02-01 00:36:38 +0530

    change folder structure

--- paytm-gatway/backend/config/database.js
+++ paytm-gatway/backend/config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "Payment"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "Geet@2000"),
+      password: env("DATABASE_PASSWORD", "postgres"),
       schema: env("DATABASE_SCHEMA", "public"),
       ssl: false,
     },
